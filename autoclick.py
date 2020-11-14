b5ef66634139b88b854af8375bc54b9ffa61527a
qet006 <30515301+qet006@users.noreply.github.com>
2020-11-14 22:53:58 +0800

    Update autoclick.py

--- autoclick.py
+++ autoclick.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 # 模拟浏览器打开网站
-saveFile("准备打开网页----")
+print("准备打开网页----")
 chrome_options = Options()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
@@ -20,7 +20,7 @@ def scut():
     browser.get('http://fizzcloud.cf/auth/login')
     # 将窗口最大化
     browser.maximize_window()
-    saveFile("打开网页了!")
+    print("打开网页了!")
     time.sleep(10)
     return
     # 格式是PEP8自动转的
@@ -36,44 +36,14 @@ def scut():
         browser.find_element_by_xpath("//*[@id='app']/div/div/div[2]/div[3]/button").click()
         print("华工申报成功")
         time.sleep(3)
-        saveFile("华工健康申报签到成功！")
+        #saveFile("华工健康申报签到成功！")
     except NoSuchElementException as e:
-        print ("NoSuchElementException!")
+        print ("华工签到代码存在异常"+str(e))
         # js = 'document.getElementById("btn").click();'
         # browser.execute_script(js)
-        saveFile("华工签到代码存在异常"+str(e))
+        #saveFile("华工签到代码存在异常"+str(e))
 
 
-def saveFile(message):
-    print(message)
-
-
-def situyun():
-    browser.get('http://situcloud.xyz/auth/login')
-    # 将窗口最大化
-    browser.maximize_window()
-    # 格式是PEP8自动转的
-    # 这里是找到输入框,发送要输入的用户名和密码,模拟登陆
-    browser.find_element_by_xpath(
-        "//*[@id='email']").send_keys(os.environ['SITUYUN_USER'])
-    browser.find_element_by_xpath(
-        "//*[@id='password']").send_keys(os.environ['SITUYUN_PASSWORD'])
-    # 在输入用户名和密码之后,点击登陆按钮
-    browser.find_element_by_xpath("//*[@id='app']/section/div/div/div/div[2]/form/div/div[5]/button").click()
-    time.sleep(10)
-    try:
-        if("明日再来" in browser.find_element_by_xpath("//*[@id='checkin-div']").text):
-            saveFile("明日再来!")
-        else:
-            # browser.find_element_by_xpath("//*[@id='checkin-div']/a").send_keys(Keys.ENTER)
-            js = 'document.getElementById("checkin-div").children[0].click();'
-            browser.execute_script(js)
-            print("司徒云打卡成功")
-        time.sleep(3)
-        saveFile("司徒云签到成功！")
-    except NoSuchElementException as e:
-        print ("NoSuchElementException!")
-        saveFile("司徒云签到代码存在异常"+str(e))
 
 if __name__ == '__main__':
     scut()

302069a3ba65d23be0a62b6c4064ab9180d94da7
qet006 <30515301+qet006@users.noreply.github.com>
2020-12-17 09:30:18 +0800

    Update autoclick.py

--- autoclick.py
+++ autoclick.py
@@ -197,7 +197,8 @@ def ekp_qd():
         else:
             log_print("***签到按钮不对!,取消EKP签到!***", 1)
         return
-    obj.click()
+    browser.find_element_by_css_selector('.ci_checkBox').click()
+    #obj.click()
     #browser.execute_script('$(".ci_checkBox").click()')
     time.sleep(2)
     obj = browser.find_element_by_css_selector('.ci_checkBox')
